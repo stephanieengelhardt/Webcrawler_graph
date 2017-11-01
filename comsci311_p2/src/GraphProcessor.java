1c4ede9dddda5d7d8e95c74cc42900f1ae037abc
Stephanie Engelhardt <sae1@iastate.edu>
2017-11-01 11:36:21 -0500

    skeleton code

--- comsci311_p2/src/GraphProcessor.java
+++ comsci311_p2/src/GraphProcessor.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 /*
  * @author Joshua Forest and Stephanie Engelhardt
  */
 public class GraphProcessor {
 
+	public GraphProcessor(String graphData) {
+		
+	}
+	
+	public int outDegree(String v) {
+		return 0;
+	}
+	
+	public ArrayList<String> bfsPath(String u, String v){
+		return null;
+	}
+	
+	public int diameter() {
+		return 0;
+	}
+	
+	public int centrality(String v) {
+		return 0;
+	}
 }
--- comsci311_p2/src/WikiCrawler.java
+++ comsci311_p2/src/WikiCrawler.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+
 /*
  * @author Joshua Forest and Stephanie Engelhardt
  */
 public class WikiCrawler {
-
+	//We MUST use this or we get 0 credit
+	private static final String BASE_URL= "https://en.wikipedia.org";
+	ArrayList<String> links;
+	int max;
+	
+	public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) {
+		
+	}
+	
+	public ArrayList<String> extractLinks(String doc){
+		return null;
+	}
+	
+	public void crawl() throws InterruptedException {
+		for(int i=0; i<max; i++) {
+			String URL= BASE_URL+links.get(i);
+			if(i%50==0) {
+				//after 50 requests you MUST use Thread.sleep() (or we get 0 credit)
+				Thread.sleep(3000);
+			}
+		}
+	}
 }
