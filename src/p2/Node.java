d2dbe837090128e59b7caac3f085b568e38895a8
sae1 <sae1@iastate.edu>
2017-11-08 19:22:55 -0600

    Update Node.java to include data to hold if it has been visited

--- src/p2/Node.java
+++ src/p2/Node.java
@@ -2,12 +2,18 @@ package p2;
 
 public class Node {
 	String data;
+	Boolean visited; 
 	
-	public Node(String data) {
+	public Node(String data, boolean visted) {
 		this.data=data;
+		this.visited=visited;
 	}
 	
 	public String getData() {
 		return data;
+	}
+	
+	public Boolean getVisited(){
+	    return visited;
 	}
 }
