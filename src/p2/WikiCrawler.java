e9f3514ab5383f388d48e0c77771da3989b96f79
Joshua Forest <joshuaforest@joshuas-air04.student.iastate.edu>
2017-11-12 12:44:59 -0600

    crawler stuff

--- src/p2/WikiCrawler.java
+++ src/p2/WikiCrawler.java
@@ -12,17 +12,19 @@ public class WikiCrawler {
 	
 	//We MUST use this or we get 0 credits
 	private static final String BASE_URL= "https://en.wikipedia.org";
-	String fileName;
-	String seedURL;
-	ArrayList<String> links;
-	ArrayList<String> topics;
-	int max;
-	Queue<String> queue= new LinkedList<String>();
+	private String fileName;
+	private String seedURL;
+	//private ArrayList<String> links;
+	private ArrayList<String> topics;
+	private int max;
+	private int count;
+	//private Queue<String> queue= new LinkedList<String>();
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		WikiCrawler w= new WikiCrawler("hello", 10, null, "test.txt");
-		ArrayList<String> test= w.extractLinks("/wiki/Iowa_State_Cyclones");
+		String t = w.getHTML("/wiki/Iowa_State_Cyclones");
+		ArrayList<String> test= w.extractLinks(t);
 		for(String s: test) {
 			System.out.println(s);
 		}	
@@ -38,59 +40,69 @@ public class WikiCrawler {
 	}
 	
 	public ArrayList<String> extractLinks(String doc) throws IOException{
-		ArrayList<String> result= new ArrayList<String>();
-		String line;
-		//FileReader fileReader= new FileReader(doc);
-		//BufferedReader br= new BufferedReader(fileReader);
-		String page = BASE_URL + doc;
-		URL url = new URL(page);
-		InputStream is = url.openStream();
-		BufferedReader br = new BufferedReader (new InputStreamReader(is));
-		while((line=br.readLine())!=null) {
-			if(line.contains("<P>") || line.contains("<p>")){
-				//found first occurrence of <P> or <p>, so start saving links
-				break;
+		ArrayList<String> result = new ArrayList<String>();	
+		if(doc.contains("<P>") && doc.contains("<p>")){
+			doc=doc.substring(Math.min(doc.indexOf("<P>"), doc.indexOf("<p>")));
 			}
+		else if(doc.contains("<P>")){
+			doc=doc.substring(doc.indexOf("<P>"));
 		}
-		boolean done=false;
-		while(!done) {
-			result.addAll(extractFromString(line));
-			if((line=br.readLine())==null) done=true;
+		else if(doc.contains("<p>")){
+			doc=doc.substring(doc.indexOf("<p>"));
 		}
-		br.close();
+		else{
+			return result;
+		}
+		
+		result=extractFromString(doc,result);
 		return result;
 	}
 	
-	private ArrayList<String> extractFromString(String line){
-		ArrayList<String> result = new ArrayList<String>();
+	private ArrayList<String> extractFromString(String line, ArrayList<String> result){
 		String link;
 		String remainingString="";
 		boolean more = false;
-		if(line.contains("<a href=\"/wiki")) { // add <a href "
+		if(line.contains("<a href=\"/wiki")) {
+			line=line.substring(line.indexOf("<a href=\"/wiki"));
 			link=line.substring(line.indexOf("/wiki"));
 			if(link.indexOf('"')>-1){
 				more=true;
 				remainingString=link.substring(link.indexOf('"'));
 			}
 			link=link.substring(0, link.indexOf('"'));
-			if(!link.contains("#") && !link.contains(":")) result.add(link);
-			if(more) result.addAll(extractFromString((remainingString)));
+			if(!link.contains("#") && !link.contains(":") && !result.contains(link)) result.add(link);
+			if(more) result=extractFromString(remainingString, result);
 		}
 		return result;
 	}
 	
+	private String getHTML(String link) throws InterruptedException, IOException{
+		String website = BASE_URL+link;
+		URL url= new URL(website);
+		InputStream read= url.openStream();
+		BufferedReader br= new BufferedReader(new InputStreamReader(read));
+		count ++;
+		if(count%50==0) {
+			//after 50 requests you MUST use Thread.sleep() (or we get 0 credit)
+			Thread.sleep(3000);
+		}
+		String line;
+		String doc="";
+		while((line=br.readLine()) != null){
+			doc+=line;
+		}
+		return doc;
+	}
+	
 	public void crawl() throws InterruptedException, IOException {
 		//only find 0 to max amount of links within this webpage
+		Queue<String> q = new LinkedList<String>();
+		LinkedList<String> l=new LinkedList<String>();
 		for(int i=0; i<max; i++) {
-			String website= BASE_URL+links.get(i);
-			URL url= new URL(website);
-			if(i%50==0) {
-				//after 50 requests you MUST use Thread.sleep() (or we get 0 credit)
-				Thread.sleep(3000);
+			String doc = getHTML(seedURL);
+			for(String s : extractLinks(doc)){
+				q.add(doc);
 			}
-			InputStream read= url.openStream();
-			BufferedReader br= new BufferedReader(new InputStreamReader(read));
-			String line=br.readLine();
 		}
 	}
 }
